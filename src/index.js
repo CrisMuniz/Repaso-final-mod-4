b9175bdc057ebe80d37bab1bb7ba6dd4fe7f1215
CrisMuniz <cristinamuniz85@gmail.com>
2023-07-22 20:00:53 +0200

    add endpoints

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const server = express();
 
 server.use(cors());
 server.use(express.json({limit: "25mb"}));
-server.set('view engine', 'ejs');
+
 
 
 
@@ -32,7 +32,7 @@ async function getConnection() {
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "root",
       password: process.env.DB_PASS,  // <-- Pon aquí tu contraseña o en el fichero /.env en la carpeta raíz
-      database: process.env.DB_NAME || "Clase",
+      database: process.env.DB_NAME || "Adakiteen",
     }
   );
 
@@ -54,32 +54,102 @@ server.listen(port, () => {
 
 // Endpoints
 
-// GET /api/items
+// GET /api/kittens/:user
+//obtener el lsitado de todos los gatitos donde user es el parametro que especifica el usuario y solo devolver lods gatos de ese usuario.
 
-server.get("/api/items", async (req, res) => {
+server.get("/api/kittens/:user", async (req, res) => {
+  const user = req.params.user;
 
-  const selectProducts = "SELECT * FROM products";
+  const select = "SELECT * FROM Kitten WHERE owner = ?";
 
   const conn = await getConnection();
 
-  const [results] = await conn.query(selectProducts);
+  const [result] = await conn.query(select, user); // las dos constantes(la query y los params)
 
-  console.log(results);
+  console.log(result);
 
   conn.end();
 
-  res.json(results);
+  res.json ({
+    info: {
+        count: result.length, //número de elementos del listado.Si pidiera mas resultados 
+    },
+    results: result //listado de gatitos
+})
 });
 
 
 
-// GET /details
+// POST /api/kittens/:user
+// insertar un nuevo gatito donde user es el usuario que esta insertando el gatito.Tambien se espera por el body de la peticion toda la info del gato a insertar.Devolver la respuesta si es exitoxa o no.
 
-server.get("/details", async (req, res) => {
+server.post("/api/kittens/:user", async (req, res) => {
+  const user = req.params.user;
+  const newKitten = req.body;
+  try{
 
-  res.render('details', {})
+  const insert = "INSERT INTO Kitten (`owner`, url, name, race, `desc`) VALUES (?, ?, ?, ?, ?);";
+  const conn = await getConnection();
+  const [result] = await conn.query(insert, [user, newKitten.url, newKitten.name, newKitten.race, newKitten.desc]); // Cogemos la constante de la query, cogemos la constante de el user y la constante de newKitten con sus respectivas propiedades que vienen del front y qfue coinciden con las columnas de nuestra BD
+  conn.end();
+
+  res.json ({
+      success: true //Puede ser true o false
+  });
+  } catch (error) {
+    res.json ({
+      success: false,//Puede ser true o false
+      message: error,
+  });
+  }
 });
 
+//PUT /api/kittens/:user/:kitten_id
+// Actualizar un gatito, donde user es el parametro que especifica el usuario al que se actualiza el gato y kitten_id es el identificador del gato a actualizar.Tambien se espera por el body toda la info del gato.Devolver la respuesta si es exitosa o no.
+server.put("/api/kittens/:user/:kitten_id", async (req, res) => {
+  const user = req.params.user;
+  const kittenId = req.params.kitten_id;
+  const {url, name, race, desc} = req.body;// hacemos destructurin del objeto(como arriba en newKitten.url, newKitten.name....)
+  try{
+
+  const update = "UPDATE Kitten SET url= ?, name= ?, race= ?, `desc`= ? WHERE id = ?";
+  const conn = await getConnection();
+  const [result] = await conn.query(update, [url, name, race, desc, kittenId]);
+  conn.end();
+
+  res.json ({
+      success: true //Puede ser true o false
+  });
+  } catch (error) {
+    res.json ({
+      success: false,//Puede ser true o false
+      message: error,
+  });
+  }
+});
+
+//DELETE /api/kittens/:user/:kitten_id
+//Eliminar un gatito donde user es el parametro que especifica que usuario está eliminando el gato y el kitten_id es el identificador del gato a eliminar. Devolver la respuesta si fue exitosa o no.
+server.delete("/api/kittens/:user/:kitten_id", async (req, res) => {
+  const user = req.params.user;
+  const kittenId = req.params.kitten_id;
+  try{
+
+  const deleteSql = "DELETE FROM Kitten WHERE id= ? and owner= ?";
+  const conn = await getConnection();
+  const [result] = await conn.query(deleteSql, [kittenId, user]);//Debemos poner el mismo orden de las contantes como en la sentencia sql
+  conn.end();
+
+  res.json ({
+      success: true //Puede ser true o false
+  });
+  } catch (error) {
+    res.json ({
+      success: false,//Puede ser true o false
+      message: error,
+  });
+  }
+});
 
 // Serv estáticos
 
